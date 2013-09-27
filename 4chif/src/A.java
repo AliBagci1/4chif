b69bc282c341d2537415ea6bb9a0d1ef808d37a3
AliBagci1 <bag14113@spengergasse.at>
2013-09-27 12:40:11 +0200

    start

--- 4chif/src/A.java
+++ 4chif/src/A.java
@@ -4,5 +4,6 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Hallo git 333");
+			System.out.println("Hallo welt");
 	}
 }
