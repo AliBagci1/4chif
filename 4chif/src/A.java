0c26b090a85dbd576712a5c161dff034ab79e428
AliBagci1 <bag14113@spengergasse.at>
2013-09-27 12:40:43 +0200

    Update A.java

--- 4chif/src/A.java
+++ 4chif/src/A.java
@@ -1,8 +1,7 @@
-
 public class A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hallo git 333");
+			System.out.println("Hallo git");
 	}
 }
